09da89df45563cf6e4a8fda9afd3de781ca6985e
Jakob-Lovato <106411094+Jakob-Lovato@users.noreply.github.com>
2022-07-03 15:56:24 -0700

    Finalize design
    Finalized plot colors, fonts, etc

--- Cocktail.R
+++ Cocktail.R
@@ -46,11 +46,12 @@ top10 <-
 top10$ingredient <- factor(top10$ingredient, levels = top10$ingredient)
 
 # Pre-set colors and fonts
-fg_col <- rgb(235, 232, 227, maxColorValue = 255)
-bg_col <- rgb(30, 30, 60, maxColorValue = 255)
-line_col <- rgb(16, 14, 46, maxColorValue = 255)
+fg_col <- rgb(230, 221, 190, maxColorValue = 255)
+bg_col <- rgb(5, 3, 32, maxColorValue = 255)
+line_col <- rgb(62, 62, 55, maxColorValue = 255)
+title_col <- rgb(252, 252, 248, maxColorValue = 255)
 body_font <- "Proxima Nova"
-title_font <- "Tamil MN"
+title_font <- "Optima"
 
 # Create plot of 10 most common ingredients
 ggplot(top10, aes(ingredient, n)) +
@@ -74,25 +75,27 @@ ggplot(top10, aes(ingredient, n)) +
         
         axis.title.x = element_text(size = 18,
                                     family = body_font, 
-                                    color = fg_col),
+                                    color = title_col),
         
         axis.title.y = element_text(size = 15,
                                     family = body_font, 
-                                    color = fg_col,
+                                    color = title_col,
                                     margin = margin(#l = 10,
-                                                    r = 10)),
+                                                    r = 20)),
         
         panel.grid.major.y = element_line(size = 0.5,
-                                          color = bg_col),
+                                          color = line_col,
+                                          linetype = "dotted"),
         
         panel.grid.minor.y = element_line(size = 0.5,
-                                          color = bg_col),
+                                          color = line_col,
+                                          linetype = "dotted"),
         
         panel.grid.major.x = element_blank(),
         axis.ticks = element_blank(),
         panel.background = element_blank(),
-        plot.background = element_rect(fill = line_col),
-        plot.margin = margin(20, 20, 20, 20)) +
+        plot.background = element_rect(fill = bg_col),
+        plot.margin = margin(25, 20, 20, 20)) +
   
   annotate(geom = "curve", 
            x = 3, 
@@ -101,27 +104,30 @@ ggplot(top10, aes(ingredient, n)) +
            yend = 0.175, 
            curvature = .3, 
            arrow = arrow(length = unit(2, "mm")), 
-           color = fg_col) +
+           color = title_col) +
   
   annotate(geom = "text", 
-           x = 3.15, 
-           y = 0.155, 
+           x = 3.1, 
+           y = 0.16, 
            label = "Gin is the most common \ningredient, being used in \nabout 18% of cocktails", 
            hjust = "left",
            family = body_font,
            #fontface = "italic",
-           size = 3.75,
-           color = fg_col,) +
+           size = 3,
+           color = title_col,) +
   
   #title
   annotate(geom = "text",
            x = 10.5,
-           y = 0.15,
-           label = "Ten Most Used \nCocktail \nIngredients",
+           y = 0.165,
+           label = "Ten Most Common \nCocktail Ingredients",
            hjust = "right",
            family = title_font,
-           fontface = "bold",
-           size = 6.5,
-           color = fg_col) +
+           fontface = "bold.italic",
+           size = 8,
+           color = title_col) +
   
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) 
+
+# save with device = cairo.pdf to embed fonts
+ggsave("cocktailplot.pdf", width = 1530, height = 1128, units = "px", scale = 1.75, device = cairo_pdf)
